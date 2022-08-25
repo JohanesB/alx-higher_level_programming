0509e6a2900c2121292bc3432dd45618912baf74
Yohanes <yohanesbacha@gmail.com>
2022-08-25 15:17:38 -0700

    3rd commit for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if __name__ == "__main__":
     arglen = len(argv) - 1
     if arglen == 0:
         print('0 arguments.')
