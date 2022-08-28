b736bb76e6f1d165a193be27217764857c9188bb
Yohanes <yohanesbacha@gmail.com>
2022-08-28 10:41:07 -0700

    second commit for task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for x in my_list:
-        print("{:d}".format(my_list[idx]))
+    if 0 < idx < len(my_list):
+        return (my_list[idx])
+    else:
+        return (None)
