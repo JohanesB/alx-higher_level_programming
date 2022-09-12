bee994b51688d22336788a5ebd175f6c1d9b0c40
Yohanes <yohanesbacha@gmail.com>
2022-09-12 12:44:08 -0700

    commit 5 for task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            new_list.append(dev)
-            return new_list
+            new_list.append(div)
+    return new_list
