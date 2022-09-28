184e6d59cf0e18e5d535990dacd1ca99656a1e1b
Yohanes <yohanesbacha@gmail.com>
2022-09-28 12:45:22 -0700

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" Function: read_file """
+"""read file"""
 
 
 def read_file(filename=""):
-    """ read from a text file and print it to the stdout """
+    """read file"""
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
