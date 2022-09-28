828f295e6246d6a71a2ef7fe60fd38919f24eb9f
Yohanes <yohanesbacha@gmail.com>
2022-09-28 12:51:34 -0700

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""read file"""
+
+"""This is a module for read_file."""
 
 
 def read_file(filename=""):
-    """read file"""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    """A function that reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding='utf-8') as f:
+        read_result = f.read()
+        print(read_result, end="")
