e677681922f7708cb45f398a68b8d19987c96923
Yohanes <yohanesbacha@gmail.com>
2022-11-03 07:21:30 -0700

    commit 9 for task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
--- Lists all the cities of CA that can be found in the DB hbtn_0d_usa
--- states contains record where name = California
--- Results ascd order by cities.id
--- no join
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
+--  lists all the cities of California that can be found in the database
+-- in hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = 1
+ORDER BY id ASC;
