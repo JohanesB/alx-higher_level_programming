ce1af6908c8a30e7b4326c02971eee698642de4a
Yohanes <yohanesbacha@gmail.com>
2022-11-03 07:08:20 -0700

    commit 5 for task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- unique_id
--- non-repetition
-CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1,
-name VARCHAR(256));
+-- Creates a table id_not_null
+-- Description: id INT def_val = 1, name VARCHAR(256)
+-- Should not fail
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
