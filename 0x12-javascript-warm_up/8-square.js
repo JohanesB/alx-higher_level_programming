be4bd268b1e93216d9140565e3cdf3e1867c1737
Yohanes <yohanesbacha@gmail.com>
2022-11-30 04:53:02 -0800

    commit 9 for task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(str.repeat(parseInt(process.argv[2])));
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
